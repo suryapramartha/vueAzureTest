88072a0f0ed4fece976d2e8c81e15a0037242d2e
Gde Surya Pramartha <suryapramartha@gmail.com>
2020-03-31 11:43:41 +0800

    second mortal

--- vue-hello-world/src/App.vue
+++ vue-hello-world/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App, Mortal"/>
+    <HelloWorld msg="Welcome to Vue.js App, Mortal"/>
   </div>
 </template>
 
